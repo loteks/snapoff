d83455bac2640044a11485cf961c29c78522e957
Steve Freeman <218478+omnidelic@users.noreply.github.com>
2022-04-07 17:51:36 -0400

    Add files via upload

---
+++ snapoff.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Remove snaps from ubuntu 22.04 and install mozilla ppa
+
+[ "$EUID" -ne 0 ] && echo "Must be root" && exit 1
+
+snap remove firefox && apt purge snapd -y && \
+echo -e 'Package: snapd\nPin: origin *\nPin-Priority: -1' | sudo tee /etc/apt/preferences.d/no_snapd  && \
+add-apt-repository ppa:mozillateam/ppa -y && \
+
+cat << EOF > /etc/apt/preferences.d/mozillateamppa
+Package: firefox*
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 501
+EOF
+
+apt update && apt install firefox -y
+
+echo -e '\n----- Welcome back to snap free ubuntu! -----\n'
+
+exit 0
